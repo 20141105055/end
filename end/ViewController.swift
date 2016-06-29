8536ac225685ff5acb9a34096f476bead04738c3
卢佳明 <571836353@qq.com>
2016-06-29 18:20:49 +0800

    第四版本：计算器
    较上一版本添加了“AC”清空键的功能，“AC”键的处理上，对编辑框型x，y同时清零。

--- end/ViewController.swift
+++ end/ViewController.swift
@@ -9,7 +9,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     var flag=0
+    var temp=0.0
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBAction func one(sender: AnyObject) {
+        x.text=x.text!+"1"
+    }
+    @IBAction func two(sender: AnyObject) {
+        x.text=x.text!+"2"
+    }
+    @IBAction func three(sender: AnyObject) {
+       x.text=x.text!+"3"
+    }
+    @IBAction func four(sender: AnyObject) {
+        x.text=x.text!+"4"
+    }
+    @IBAction func five(sender: AnyObject) {
+        x.text=x.text!+"5"
+    }
+    @IBAction func six(sender: AnyObject) {
+        x.text=x.text!+"6"
+    }
+ 
+    @IBAction func seven(sender: AnyObject) {
+        x.text=x.text!+"7"
+    }
+    @IBAction func eight(sender: AnyObject) {
+        x.text=x.text!+"8"
+    }
+    @IBAction func nine(sender: AnyObject) {
+        x.text=x.text!+"9"
+    }
+    @IBAction func zero(sender: AnyObject) {
+        x.text=x.text!+"0"
+    }
+    @IBAction func add(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=1
+        }
+        flag=1
+    }
+    @IBAction func sub(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=2
+        }
+        flag=2
+        
+    }
+    @IBAction func mul(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=3
+        }
+        flag=3
+    }
+    @IBAction func div(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=4
+        }
+        flag=4
+    }
+    @IBAction func AC(sender: AnyObject) {
+        x.text=""
+        y.text=""
+    }
+    @IBAction func delet(sender: AnyObject) {
+    }
+    @IBAction func calculation(sender: AnyObject) {
+        switch flag{
+        case 1:
+            temp=temp+(Double)(x.text!)!
+            x.text=""
+            y.text="\(temp)"
+          //  y.text=x.text
+            break
+        case 2:
+            temp=temp-(Double)(x.text!)!
+            x.text=""
+            y.text="\(temp)"
+           // y.text=x.text
+            break
+        case 3:
+            temp=temp*(Double)(x.text!)!
+            x.text=""
+            y.text="\(temp)"
+            //y.text=x.text
+            break
+        case 4:
+            if (Double)(x.text!)==0{
+                let alertView=UIAlertView()
+                alertView.title="noise"
+                alertView.message="0不可以做除数"
+                alertView.addButtonWithTitle("确认")
+                alertView.show()
+            }
+            temp=temp/(Double)(x.text!)!
+            x.text=""
+            y.text="\(temp)"
+           // y.text=x.text
+            break
+        default:
+            break
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
